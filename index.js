a61d015f7d09e96627754fae7f9682ff3e69bdd2
Collins Gichuki <gichukimuchiri80@gmail.com>
2024-04-04 13:22:22 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching (drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch (drivers, letter) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letter.toLowerCase()));
+}
+
+function matchName (drivers, arg) {
+    return drivers.filter(driver => driver.name.toLowerCase() === arg.toLowerCase())
+}
